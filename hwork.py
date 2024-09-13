dd53a58794f7daf8cf06f2ae608afdad2b22132c
Abdullo <147158343+Abdushahidov07@users.noreply.github.com>
2024-09-13 16:50:30 +0500

    hwork

---
+++ hwork.py
@@ -0,0 +1,42 @@
+# Task 1
+# class Person:
+#     def __init__(self, name):
+#         self.name=name
+#     def hi_hello(self):
+#         print(f"Hello, my name is {self.name}")
+# per1=Person(input())
+# per1.hi_hello()
+
+# Task 2
+# class Ploshad:
+#     def __init__(self, a, b):
+#         self.a=a
+#         self.b=b
+#     def ploshadkvadrata(self):
+#         print(f"Площадь квадрата {self.a*self.b}")
+# kvadrat = Ploshad(int(input()),int(input()))
+# kvadrat.ploshadkvadrata()
+
+# Task 3
+# class ploshadfigur:
+#     def __init__(self, r):
+#         self.r=r
+#         pass
+#     def kryg(self):
+#         print(f"Площадь круга: {3.14*(self.r**2) }")
+# figyri =  ploshadfigur(int(input("r = ")))
+# figyri.kryg()
+
+# Task 4
+
+# class Car:
+#     total_car= 0
+#     def __init__(self):
+#         Car.total_car += 1
+#     @classmethod
+#     def totalcar(cls):
+#         return cls.total_car
+# car1 = Car()
+# car2 = Car()
+# car3 = Car()
+# print(Car.totalcar())
